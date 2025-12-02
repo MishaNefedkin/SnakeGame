28780a25e5ce5f76a86396329723588ee8fdcd0d
MishaNefedkin <mishanef06@gmail.com>
2025-12-02 16:20:43 +0100

    dodalen pliki kodu

---
+++ Obstakel.cs
@@ -0,0 +1,15 @@
+namespace SnakeGame;
+
+public class Obstakel
+
+{
+
+    public int Xpos { get; set; }
+
+    // ?
+
+    public ConsoleColor schermKleur { get; set; }
+
+    public string karacter { get; set; }
+
+}
